3366256dba31ec2b4a043a2be403182ada227774
Pedro Pablo Kinil Chi <pkinil@nasa.com.mx>
2023-11-23 08:30:09 -0600

    Version 1.0.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HomeComponent } from './modules/home/home.component';
+import { SearchComponent } from './modules/search/search.component';
+import { BlackBackgroundDirective } from './shared/directives/black-background.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    SearchComponent,
+    BlackBackgroundDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/directives/black-background.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appBlackBackground]'
+})
+export class BlackBackgroundDirective {
+
+  constructor() { }
+
+}
